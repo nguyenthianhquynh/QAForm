82fe7a7a4ed0e718ca07dcb3bc039cd6cfd428fa
quynh-nta <nguyenthianhquynh116@gmail.com>
2024-03-02 15:13:15 +0700

    add route file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { AddQuestionComponent } from './pages/add-question/add-question.component';
+import { BuilderComponent } from './pages/builder/builder.component';
+import { AnswersComponent } from './pages/answers/answers.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: AddQuestionComponent,
+    },
+
+    //http://localhost:4200/form/builder
+    {
+        path : 'form/builder', component: BuilderComponent
+    },
+    //http://localhost:4200/form/answers
+    {
+        path: 'form/answers', component: AnswersComponent
+    },  
+    { path: '**', redirectTo: '', pathMatch: 'full' },
+];
